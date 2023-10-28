1b4062da6a4e213961ee2b80bfc7fd83d55c212e
Emre Ozkaya <emre.ozkaya@tokeninc.com>
2023-10-28 17:46:47 +0300

    fix : cookie token send operation without http only

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -68,8 +68,8 @@ const register = async (_req, _res) => {
 			send_email("ozthefur@gmail.com", "Confirm account 🤕", "confirm_account", confirm_credential);
 			//------------------
 			console.info(chalk.green.bold(`${getTimestamp()} Status Code : 201 -- Info : User Created -- ID : ${user_registered._id}`));
-			// _res.cookie("jwt", confirm_token, {httpOnly: false, maxAge: 60 * 60, secure: true})
-			return _res.status(201).json({message: "User Created , Please Confirm Your Email", confirm_token});
+			_res.cookie("confirm_token", confirm_token, {maxAge: 60 * 60, secure: true})
+			return _res.status(201).json({message: "User Created , Please Confirm Your Email"});
 		} catch (error) {
 			console.error(chalk.bold(`${getTimestamp()} Status Code : 400 -- Error : Invalid User Data -- Service : Register`));
 			return _res.status(400).json({message: "Invalid User Data"});
