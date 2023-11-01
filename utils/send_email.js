4a88437ed2958ac6c83b3356709f6831d836e0ff
Emre Ozkaya <emre.ozkaya@tokeninc.com>
2023-11-01 20:17:01 +0300

    -

--- utils/send_email.js
+++ utils/send_email.js
@@ -5,6 +5,7 @@ dotenv.config()
 const transporter = nodemailer.createTransport({
 	port: Number(process.env.EMAIL_PORT),
 	host: process.env.EMAIL_HOST,
+	secure: true,
 	auth: {
 		user: process.env.EMAIL_USER,
 		pass: process.env.EMAIL_PASSWORD,
