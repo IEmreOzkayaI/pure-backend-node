0282a8ed332c0610f733f354b023994923bf2590
Furkan Özdemir <ozthefur@gmail.com>
2023-11-09 20:13:42 +0300

    update allowed_origins

--- config/allowed_origins.js
+++ config/allowed_origins.js
@@ -1,6 +1,7 @@
 const allowed_origins = [
-    'http://localhost:3001',
-    'https://the-pure-tech-home-page.netlify.app',
-]
+  "http://localhost:3001",
+  "https://the-pure-tech-home-page.netlify.app",
+  "http://127.0.0.1:3001",
+];
 
 export default allowed_origins;
