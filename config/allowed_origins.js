409a787707314f323b1c8980cb7fd0ea47a3b26d
Emre <72611040+IEmreOzkayaI@users.noreply.github.com>
2024-01-08 02:08:03 +0300

    Update allowed_origins.js

--- config/allowed_origins.js
+++ config/allowed_origins.js
@@ -2,6 +2,9 @@ const allowed_origins = [
   "http://localhost:3001",
   "https://the-pure-tech-home-page.netlify.app",
   "http://127.0.0.1:3001",
+  "https://pure-backend-node-5rdbxjkp1-iemreozkayai.vercel.app",
+  "https://pure-backend-node.vercel.app",
+  "https://pure-backend-node-5rdbxjkp1-iemreozkayai.vercel.app"
 ];
 
 export default allowed_origins;
