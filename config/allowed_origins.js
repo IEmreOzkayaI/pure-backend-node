64dd1df6fd9f0daa30fe8bcc353ef25c49c1c002
Emre Ozkaya <emre.ozkaya@tokeninc.com>
2024-01-29 16:38:27 +0300

    Update: domain update

--- config/allowed_origins.js
+++ config/allowed_origins.js
@@ -4,7 +4,8 @@ const allowed_origins = [
     "http://127.0.0.1:3001",
     "http://localhost:8080",
     "https://the-pure.netlify.app",
-    "https://the-pure.tech"
+    "https://the-pure.tech",
+    "https://sea-lion-app-skma6.ondigitalocean.app/login"
 ];
 
 export default allowed_origins;
